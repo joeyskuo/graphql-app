87fb3374b4a827f536ce94c633808a9c0ee51d06
Joey <joeyskuo@gmail.com>
2020-02-02 21:15:01 -0800

    added typeDefs and resolvers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type definition ( schema )
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'Hello GraphQL!'
+        },
+        name() {
+            return 'GraphQL 4000'
+        }
+    }
+}
