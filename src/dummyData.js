c3db4786d730638425d04a971928d495931d4d72
Joey <joeyskuo@gmail.com>
2020-02-05 09:54:21 -0800

    created dummy user data

---
+++ src/dummyData.js
@@ -0,0 +1,18 @@
+
+export const data = {
+    users: [{
+        id: '1',
+        name: 'Bob',
+        email: 'bob@example.com'
+    },
+    {
+        id: '2',
+        name: 'John',
+        email: 'john@example.com'
+    },
+    {
+        id: '3',
+        name: 'Kay',
+        email: 'kay@example.com'
+    }]
+}
