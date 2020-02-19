f9357dc3cf4d4f33b57b241b855d80de9684e63b
Joey <joeyskuo@gmail.com>
2020-02-19 15:40:49 -0800

    context implemented for dummy data

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const resolvers = {
                 email: 'john@example.com'
             }
         },
-        users(parent, args, ctx, info) {
+        users(parent, args, { data }, info) {
             if(!args.query) {
                 return data.users;
             }
@@ -21,7 +21,7 @@ const resolvers = {
                 return user.name.toLowerCase().includes(args.query.toLowerCase())
             })
         },
-        posts(parent, args, ctx, info) {
+        posts(parent, args, { data }, info) {
             if(!args.query) {
                 return data.posts
             }
@@ -40,7 +40,7 @@ const resolvers = {
         }
     },
     Mutation: {
-        createUser(parent, args, ctx, info) {
+        createUser(parent, args, { data }, info) {
             const emailTaken = data.users.some((user) => user.email === args.input.email)
 
             if(emailTaken) {
@@ -56,7 +56,7 @@ const resolvers = {
 
             return user
         },
-        deleteUser(parent, args, ctx, info) {
+        deleteUser(parent, args, { data }, info) {
             const userIndex = data.users.findIndex((user) => user.id === args.id);
 
             if(userIndex === -1) {
@@ -85,7 +85,7 @@ const resolvers = {
 
             return targetUser[0];
         },
-        createPost(parent, args, ctx, info) {
+        createPost(parent, args, { data }, info) {
             const userExists = data.users.some((user) => user.id === args.input.author);
 
             if(!userExists) {
@@ -101,7 +101,7 @@ const resolvers = {
 
             return post
         },
-        deletePost(parent, args, ctx, info) {
+        deletePost(parent, args, { data }, info) {
             const postIndex = data.posts.findIndex((post) => post.id === args.id);
 
             if(postIndex === -1) {
@@ -116,7 +116,7 @@ const resolvers = {
 
             return targetPost[0];
         },
-        createComment(parent, args, ctx, info) {
+        createComment(parent, args, { data }, info) {
             const userExists = data.users.some((user) => user.id === args.input.author);
             const postExists = data.posts.some((post) => post.id === args.input.post);
 
@@ -136,7 +136,7 @@ const resolvers = {
 
             return comment
         },
-        deleteComment(parent, args, ctx, info) {
+        deleteComment(parent, args, { data }, info) {
             const commentIndex = data.comments.findIndex((comment) => comment.id === args.id);
 
             if(commentIndex === -1) {
@@ -150,36 +150,36 @@ const resolvers = {
         }
     },
     Post: {
-        author(parent, args, ctx, info) {
+        author(parent, args, { data }, info) {
             return data.users.find((user) => {
                 return user.id === parent.author
             })
         },
-        comments(parent, args, ctx, info) {
+        comments(parent, args, { data }, info) {
             return data.comments.filter((comment) => {
                 return comment.post === parent.id
             })
         }
     },
     User: {
-        posts(parent, args, ctx, info) {
+        posts(parent, args, { data }, info) {
             return data.posts.filter((post) => {
                 return post.author === parent.id
             })
         },
-        comments(parent, args, ctx, info) {
+        comments(parent, args, { data }, info) {
             return data.comments.filter((comment) => {
                 return comment.author === parent.id
             })
         }
     },
     Comment: {
-        author(parent, args, ctx, info) {
+        author(parent, args, { data }, info) {
             return data.users.find((user) => {
                 return user.id === parent.author
             })
         },
-        post(parent, args, ctx, info) {
+        post(parent, args, { data }, info) {
             return data.posts.find((post) => {
                 return post.id === parent.post
             })
@@ -189,7 +189,10 @@ const resolvers = {
 
 const server = new GraphQLServer({
     typeDefs : './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: {
+        data
+    }
 })
 
 server.start(() => {
