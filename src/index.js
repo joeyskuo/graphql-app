732520c0e8578d478efd0112fd098aa741735d88
Joey <joeyskuo@gmail.com>
2020-02-19 15:35:59 -0800

    typedefs moved to new schema file

--- src/index.js
+++ src/index.js
@@ -2,67 +2,6 @@ import { GraphQLServer } from 'graphql-yoga';
 import { data } from './dummyData';
 import uuidv4 from 'uuid/v4';
 
-// Type definition ( schema )
-const typeDefs = `
-    type Query {
-        me: User!
-        users(query: String): [User!]!
-        post: Post!
-        posts(query: String): [Post!]!
-    }
-
-    type Mutation {
-        createUser(input: CreateUserInput!): User!
-        deleteUser(id: ID!): User!
-        createPost(input: CreatePostInput!): Post!
-        deletePost(id: ID!): Post!
-        createComment(input: CreateCommentInput!): Comment!
-        deleteComment(id: ID!): Comment!
-    }
-
-    input CreateUserInput {
-        name: String!
-        email: String!
-    }
-
-    input CreatePostInput {
-        title: String!
-        body: String!
-        published: Boolean!
-        author: ID!
-    }
-
-    input CreateCommentInput {
-        text: String!
-        author: ID!
-        post: ID!
-    }
-
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-        posts: [Post!]!
-        comments: [Comment!]!
-    }
-
-    type Post {
-        id: ID!
-        title: String!
-        body: String!
-        published: Boolean!
-        author: User!
-        comments: [Comment!]!
-    }
-
-    type Comment {
-        id: ID!
-        text: String!
-        author: User!
-        post: Post!
-    }
-`
-
 // Resolvers
 const resolvers = {
     Query: {
@@ -249,7 +188,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs : './src/schema.graphql',
     resolvers
 })
 
